2414ef1a308a4e9fc2e3f6210d69c2f5f436a698
Basraoui101 <abdoobas101@gmail.com>
2022-01-02 00:51:47 +0100

    abdeladim controllers update

--- controller/Infirmier.php
+++ controller/Infirmier.php
@@ -2,32 +2,26 @@
 class Infirmier extends Employe
 {
 
-    public $con ;
+    public $con;
     public $req;
-
     /**
      * Default constructor
      */
-    public function __construct($c,$nc,$dn,$adr,$sx,$tl,$eml,$pwr,$rol)
+    public function __construct($c, $nc, $dn, $adr, $sx, $tl, $eml, $pwr, $rol)
     {
-        parent::__construct($c,$nc,$dn,$adr,$sx,$tl,$eml,$pwr,$rol);
-        $con = new PDO("","","");
+        parent::__construct($c, $nc, $dn, $adr, $sx, $tl, $eml, $pwr, $rol);
+        $con = new PDO("mysql:host=localhost;dbname=centresante", "b1sra0u1", "m@roc2OO1mark5edith");
     }
 
-    
-    public function ModifierRDV( $RDV)
+
+    public function ModifierRDV($RDV)
     {
-      try{
-          
-      }catch(Exception $ex){
-        echo "$ex";
-      }
     }
 
     /**
      * @param  $RDV
      */
-    public function SupprimerRDV( $RDV)
+    public function SupprimerRDV($RDV)
     {
         // TODO implement here
     }
@@ -35,7 +29,7 @@ class Infirmier extends Employe
     /**
      * @param  $RDV
      */
-    public function AjouterRDV( $RDV)
+    public function AjouterRDV($RDV)
     {
         // TODO implement here
     }
@@ -45,21 +39,48 @@ class Infirmier extends Employe
      */
     public function ListerRDV()
     {
-        // TODO implement here
+        try {
+            $c = $this->connect();
+            if ($c != null) {
+                $req = 'SELECT * FROM `rdv`';
+                $res =$c->query($req);
+                foreach($res as $var)
+                return new RDV();
+            } else
+                echo "probleme in the ListerRDV Method";
+        } catch (Exception $ex) {
+            echo "$ex";
+        }
     }
 
     /**
      * @param  $RDV
      */
-    public function AfficherRDV( $RDV)
+    public function AfficherRDV($RDV)
     {
-        // TODO implement here
+        try {
+            $c = $this->connect();
+            if ($c != null) {
+                $req = 'SELECT `Id_rdv`, `Date_RDV`, `Heure_RDV`, `Objet`, `Cin_employe`, `Cin_patient` FROM `rdv` WHERE ' +
+                    '`Id_rdv`=' . "$RDV" . '';
+                $c->query($req);
+                foreach ($c as $var) {
+                    return new RDV($var[1], $var[2], $var[3], $var[4], $var[5], $var[6]);
+                }
+
+                # code...
+            } else {
+                echo "nothing in the RDV Table";
+            }
+        } catch (Exception $ex) {
+            echo "$ex";
+        }
     }
 
     /**
      * @param  $Patient
      */
-    public function AjouterPatient( Patient $Patient )
+    public function AjouterPatient(Patient $Patient)
     {
         $req = "INSERT INTO `` VALUES($Patient->nom_complet)";
     }
@@ -67,7 +88,7 @@ class Infirmier extends Employe
     /**
      * @param  $Patient
      */
-    public function ModiferPatient( $Patient)
+    public function ModiferPatient($Patient)
     {
         // TODO implement here
     }
@@ -77,7 +98,7 @@ class Infirmier extends Employe
      */
     public function ChercherPatient(null $Patient)
     {
-       req =  "SELECT * FROM `` WHERE $Patient->";
+        req =  "SELECT * FROM `` WHERE $Patient->";
     }
 
     /**
@@ -85,7 +106,5 @@ class Infirmier extends Employe
      */
     public function ListerPatients()
     {
-       
     }
-
 }
--- controller/RDV.php
+++ controller/RDV.php
@@ -5,18 +5,19 @@ declare(strict_types=1);
 
 class RDV
 {
-
     public $date_RDV;
     public $heure_rdv;
     public $objet;
     public $id_infirmiere;
     public $etat_RDV;
+    private $id_RDV;
 
     /**
      * Default constructor
      */
-    public function __construct($date,$time,$obj,$id_inf,$stat)
+    public function __construct($id_rdv, $date, $time, $obj, $id_inf, $stat)
     {
+        $this->id_RDV = $id_rdv;
         $this->date_RDV = $date;
         $this->heure_RDV = $time;
         $this->objet = $obj;

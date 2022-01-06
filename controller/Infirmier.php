557089eb732e3e180d06b4386dcf63ce23528cce
Basraoui101 <abdoobas101@gmail.com>
2022-01-06 08:07:46 +0100

    testing infirmier

--- controller/Infirmier.php
+++ controller/Infirmier.php
@@ -1,110 +1,133 @@
 <?php
-class Infirmier extends Employe
-{
 
-    public $con;
-    public $req;
-    /**
-     * Default constructor
-     */
-    public function __construct($c, $nc, $dn, $adr, $sx, $tl, $eml, $pwr, $rol)
+trait myconnect{
+   public  $con;
+    public function connect()
     {
-        parent::__construct($c, $nc, $dn, $adr, $sx, $tl, $eml, $pwr, $rol);
-        $con = new PDO("mysql:host=localhost;dbname=centresante", "b1sra0u1", "m@roc2OO1mark5edith");
-    }
-
-
-    public function ModifierRDV($RDV)
-    {
-    }
-
-    /**
-     * @param  $RDV
-     */
-    public function SupprimerRDV($RDV)
-    {
-        // TODO implement here
-    }
-
-    /**
-     * @param  $RDV
-     */
-    public function AjouterRDV($RDV)
-    {
-        // TODO implement here
-    }
-
-    /**
-     * 
-     */
-    public function ListerRDV()
-    {
-        try {
-            $c = $this->connect();
-            if ($c != null) {
-                $req = 'SELECT * FROM `rdv`';
-                $res =$c->query($req);
-                foreach($res as $var)
-                return new RDV();
-            } else
-                echo "probleme in the ListerRDV Method";
-        } catch (Exception $ex) {
-            echo "$ex";
+        try 
+        { 
+            $con= new PDO("mysql:host=localhost;dbname=centresante","b1sra0u1","root");
+            echo "connected";
+            return $con;
         }
-    }
-
-    /**
-     * @param  $RDV
-     */
-    public function AfficherRDV($RDV)
-    {
-        try {
-            $c = $this->connect();
-            if ($c != null) {
-                $req = 'SELECT `Id_rdv`, `Date_RDV`, `Heure_RDV`, `Objet`, `Cin_employe`, `Cin_patient` FROM `rdv` WHERE ' +
-                    '`Id_rdv`=' . "$RDV" . '';
-                $c->query($req);
-                foreach ($c as $var) {
-                    return new RDV($var[1], $var[2], $var[3], $var[4], $var[5], $var[6]);
-                }
-
-                # code...
-            } else {
-                echo "nothing in the RDV Table";
-            }
-        } catch (Exception $ex) {
-            echo "$ex";
+        catch (Exception $e)
+        {
+            echo "connection failed $e";
         }
-    }
-
-    /**
-     * @param  $Patient
-     */
-    public function AjouterPatient(Patient $Patient)
-    {
-        $req = "INSERT INTO `` VALUES($Patient->nom_complet)";
-    }
-
-    /**
-     * @param  $Patient
-     */
-    public function ModiferPatient($Patient)
-    {
-        // TODO implement here
-    }
-
-    /**
-     * @param null $Patient
-     */
-    public function ChercherPatient(null $Patient)
-    {
-        req =  "SELECT * FROM `` WHERE $Patient->";
-    }
-
-    /**
-     * 
-     */
-    public function ListerPatients()
-    {
     }
 }
+
+class Infirmier #extends Employe
+{
+    
+ public $req;
+    
+    // public function __construct($c, $nc, $dn, $adr, $sx, $tl, $eml, $pwr, $rol)
+    // {
+    //     parent::__construct($c, $nc, $dn, $adr, $sx, $tl, $eml, $pwr, $rol);
+       
+    // }
+
+
+ use myconnect;
+
+        public function __construct()
+        {
+
+        }
+    // public function ModifierRDV($RDV)
+    // {
+    // }
+
+    // /**
+    //  * @param  $RDV
+    //  */
+    // public function SupprimerRDV($RDV)
+    // {
+    //     // TODO implement here
+    // }
+
+    // /**
+    //  * @param  $RDV
+    //  */
+    // public function AjouterRDV($RDV)
+    // {
+    //     // TODO implement here
+    // }
+
+    // /**
+    //  * 
+    //  */
+    // public function ListerRDV()
+    // {
+    //     try {
+    //         $c = $this->connect();
+    //         if ($c != null) {
+    //             $req = 'SELECT * FROM `rdv`';
+    //             $res =$c->query($req);
+    //             foreach($res as $var)
+    //             return new RDV();
+    //         } else
+    //             echo "probleme in the ListerRDV Method";
+    //     } catch (Exception $ex) {
+    //         echo "$ex";
+    //     }
+    // }
+
+    // /**
+    //  * @param  $RDV
+    //  */
+    // public function AfficherRDV($RDV)
+    // {
+    //     try {
+    //         $c = $this->connect();
+    //         if ($c != null) {
+    //             $req = 'SELECT `Id_rdv`, `Date_RDV`, `Heure_RDV`, `Objet`, `Cin_employe`, `Cin_patient` FROM `rdv` WHERE ' +
+    //                 '`Id_rdv`=' . "$RDV" . '';
+    //             $c->query($req);
+    //             foreach ($c as $var) {
+    //                 return new RDV($var[1], $var[2], $var[3], $var[4], $var[5], $var[6]);
+    //             }
+
+    //             # code...
+    //         } else {
+    //             echo "nothing in the RDV Table";
+    //         }
+    //     } catch (Exception $ex) {
+    //         echo "$ex";
+    //     }
+    // }
+
+    // /**
+    //  * @param  $Patient
+    //  */
+    // public function AjouterPatient(Patient $Patient)
+    // {
+    //     $req = "INSERT INTO `` VALUES($Patient->nom_complet)";
+    // }
+
+    // /**
+    //  * @param  $Patient
+    //  */
+    // public function ModiferPatient($Patient)
+    // {
+    //     // TODO implement here
+    // }
+
+    // /**
+    //  * @param null $Patient
+    //  */
+    // // public function ChercherPatient(null $Patient)
+    // // {
+    // //     # =  "SELECT * FROM `` WHERE $Patient->";
+    // // }
+
+    // /**
+    //  * 
+    //  */
+    // public function ListerPatients()
+    // {
+    // }
+}
+
+?>

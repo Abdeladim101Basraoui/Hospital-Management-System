fae504b0e1e49d0d2fd7be6104763f1bd53456ec
Basraoui101 <abdoobas101@gmail.com>
2022-01-07 07:09:30 +0100

    the calendar Dispo dates for making an appointment is READYgit add controller/calendrier_RDV.php

--- controller/calendrier_RDV.php
+++ controller/calendrier_RDV.php
@@ -1,16 +1,10 @@
 <?php
 
-declare(strict_types=1);
-
-
 class calendrier_RDV
 {
-use myconnect;
     public $date_calendrier;
-public $id_RDV;
+    public $id_RDV;
     public $heure_calendrier;
-
-    // public array $est inclus;
 
     /**
      * Default constructor
@@ -20,7 +14,22 @@ public $id_RDV;
         // ...
     }
 
-public function dateDispo(){
-return; #date dispo;
-}
+    public static function dateDispo()
+    {
+        try {
+            $dates = array();
+            $c = Infirmier::connect();
+            $x = 0;
+            if ($c != null) {
+                $res = $c->query("SELECT `Date_calendrier_RDV`, `Heure_Calendrier_RDV`FROM `calendrier_rdv` WHERE id_rdv is null");
+                foreach ($res as $var) {
+                    $dates[] = array($var[0], $var[1]);
+                    echo $dates[$x][0]." ===== ".$dates[$x][1]."\n";
+                    $x++;
+                }
+            }
+        } catch (Exception $th) {
+            echo "$th";
+        }
+    }
 }

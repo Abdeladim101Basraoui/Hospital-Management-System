a6194c98329c51ef97eae3714167bd21ff82c68a
Basraoui101 <abdoobas101@gmail.com>
2022-01-08 12:01:34 +0100

    updates

--- controller/test.php
+++ controller/test.php
@@ -1,4 +1,23 @@
 <?php
 
+include('Infirmier.php');
+function show_doctors()
+{
+    try {
+        $c = Infirmier::connect();
+        if ($c != null) {
+            //   $res =$c->query("SELECT COUNT(*)  FROM `medecin`");
+            //   echo "<script>alert($res)</script>";
+        }
+    } catch (Exception $th) {
+        echo "<script>alert($th);</script>";
+    }
+}
+$F = new Infirmier(null,null,null,null,null,null,null,null,null);
+foreach($F->afficherRDV() as $g)
+{
+    
+}
+
 
 ?>

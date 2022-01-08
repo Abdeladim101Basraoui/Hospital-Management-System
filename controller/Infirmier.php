56a475e9b7e52530cf9c7ad20f4beba798498c5e
Basraoui101 <abdoobas101@gmail.com>
2022-01-08 12:22:16 +0100

    trait my connect working dont't Fword touch it

--- controller/Infirmier.php
+++ controller/Infirmier.php
@@ -2,7 +2,6 @@
 #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
 trait myconnect
 {
-    public  $con;
     public static function connect()
     {
         try {
@@ -37,119 +36,119 @@ trait myconnect
 }
 
 
-class Infirmier extends Employe
-{
+// --------------------------------------------
 
-    public $req;
-    use myconnect;
+// class Infirmier extends Employe
+// {
+//     use myconnect;
 
-    public function __construct($c,$nc,$dn,$adr,$sx,$tl,$eml,$pwr,$rol)
-    {
-        parent::__construct($c,$nc,$dn,$adr,$sx,$tl,$eml,$pwr,$rol);
-    }
+//     public function __construct($c,$nc,$dn,$adr,$sx,$tl,$eml,$pwr,$rol)
+//     {
+//         parent::__construct($c,$nc,$dn,$adr,$sx,$tl,$eml,$pwr,$rol);
+//     }
   
-    /**
-     * show all the patients
-     */
-    public function afficherRDV()
-    {
-        try {
+//     /**
+//      * show all the patients
+//      */
+//     public function afficherRDV()
+//     {
+//         try {
 
-            $this->ListerRDV("not null", "Cin_Patient");
-        } catch (Exception $ex) {
-            echo "$ex";
-        }
-    }
+//             $this->ListerRDV("not null", "Cin_Patient");
+//         } catch (Exception $ex) {
+//             echo "$ex";
+//         }
+//     }
 
 
-    /**
-     * @param  $col = column name
-     * @param  $val = value to search for
-     */
-    public function ListerRDV($val, $col = "Cin_patient")
-    {
-        $tbl = array();
+//     /**
+//      * @param  $col = column name
+//      * @param  $val = value to search for
+//      */
+//     public function ListerRDV($val, $col = "Cin_patient")
+//     {
+//         $tbl = array();
 
-        try {
-            $c = self::connect();
-            if ($c != null) {
-                $req = "SELECT * FROM `rdv` WHERE $col='$val'";
-                if ($val === "null" || $val === "not null") {
-                    $req = "SELECT * FROM `rdv` WHERE $col is $val";
-                }
+//         try {
+//             $c = self::connect();
+//             if ($c != null) {
+//                 $req = "SELECT * FROM `rdv` WHERE $col='$val'";
+//                 if ($val === "null" || $val === "not null") {
+//                     $req = "SELECT * FROM `rdv` WHERE $col is $val";
+//                 }
               
-                $res = $c->query($req);
-                // foreach ($res as  $val) {
-                //     $tbl[] = array($val[0],$val[1],$val[2],$val[3],$val[4],$val[5]);
-                // }
-                return $res;
-            }
-        } catch (Exception $ex) {
-            echo "$ex";
-        }
-    }
+//                 $res = $c->query($req);
+//                 // foreach ($res as  $val) {
+//                 //     $tbl[] = array($val[0],$val[1],$val[2],$val[3],$val[4],$val[5]);
+//                 // }
+//                 return $res;
+//             }
+//         } catch (Exception $ex) {
+//             echo "$ex";
+//         }
+//     }
 
-    public function AjouterRDV($r)
-    {
-        $c = self::connect();
-        if ($c != NULL) {
-            $sql = "INSERT INTO rdv(Date_RDV, Heure_RDV, Objet, Cin_employe, Cin_patient) VALUES ('$r->date_RDV','$r->heure_rdv','$r->objet','$this->CIN','$r->cin_patient')";
-            $v = $c->prepare($sql);
-            $v->execute();
-            return true;
-        }
-    }
-    public function SupprimerRDV($id)
-    {
-        $c = self::connect();
-        if ($c != NULL) {
-            $sql = "DELETE FROM rdv WHERE Id_rdv = '$id'";
-            $v = $c->prepare($sql);
-            $v->execute();
-            return true;
-        }
-    }
-
-
+//     public function AjouterRDV($r)
+//     {
+//         $c = self::connect();
+//         if ($c != NULL) {
+//             $sql = "INSERT INTO rdv(Date_RDV, Heure_RDV, Objet, Cin_employe, Cin_patient) VALUES ('$r->date_RDV','$r->heure_rdv','$r->objet','$this->CIN','$r->cin_patient')";
+//             $v = $c->prepare($sql);
+//             $v->execute();
+//             return true;
+//         }
+//     }
+//     public function SupprimerRDV($id)
+//     {
+//         $c = self::connect();
+//         if ($c != NULL) {
+//             $sql = "DELETE FROM rdv WHERE Id_rdv = '$id'";
+//             $v = $c->prepare($sql);
+//             $v->execute();
+//             return true;
+//         }
+//     }
 
 
-    public function ListerPatients()
-    {
-        $c = self::connect();
-        if ($c != null) {
-            return $c->query("SELECT * FROM `patient`");
-        }
-    }
 
-    public function AjouterPatient($pat)
-    {
-        $c = self::connect();
-        if ($c != null) {
-            $sql = "INSERT INTO patient (Cin_patient, Nom_complet, Date_naissance, Addresse, Sexe, Tel, Email, Password, Historique, Cin_employe) VALUES ('$pat->CIN', '$pat->nom_complet', '$pat->date_naissance', '$pat->addresse', '$pat->sexe', '$pat->tel', '$pat->email', '$pat->password', '$pat->historique', '$this->CIN')";
-            $query = $c->prepare($sql);
-            $query->execute();
-            return true;
-        } else {
-            echo "probleme de connexion";
-            return false;
-        }
-    }
 
-    // /**
-    //  * @param  $Patient
-    //  */
-    // public function ModifierPatient($Patient)
-    // {
-    //     try {
-    //         $c = self::connect();
-    //         if ($c != NULL) {
-    //             $sql = "";
-    //             $v = $c->prepare($sql);
-    //             $v->execute();
-    //             return true;
-    //         }
-    //     } catch (Exception $th) {
-    //         echo "$th";
-    //     }
-    // }
-}
+//     public function ListerPatients()
+//     {
+//         $c = self::connect();
+//         if ($c != null) {
+//             return $c->query("SELECT * FROM `patient`");
+//         }
+//     }
+
+//     public function AjouterPatient($pat)
+//     {
+//         $c = self::connect();
+//         if ($c != null) {
+//             $sql = "INSERT INTO patient (Cin_patient, Nom_complet, Date_naissance, Addresse, Sexe, Tel, Email, Password, Historique, Cin_employe) VALUES ('$pat->CIN', '$pat->nom_complet', '$pat->date_naissance', '$pat->addresse', '$pat->sexe', '$pat->tel', '$pat->email', '$pat->password', '$pat->historique', '$this->CIN')";
+//             $query = $c->prepare($sql);
+//             $query->execute();
+//             return true;
+//         } else {
+//             echo "probleme de connexion";
+//             return false;
+//         }
+//     }
+
+//     // /**
+//     //  * @param  $Patient
+//     //  */
+//     // public function ModifierPatient($Patient)
+//     // {
+//     //     try {
+//     //         $c = self::connect();
+//     //         if ($c != NULL) {
+//     //             $sql = "";
+//     //             $v = $c->prepare($sql);
+//     //             $v->execute();
+//     //             return true;
+//     //         }
+//     //     } catch (Exception $th) {
+//     //         echo "$th";
+//     //     }
+//     // }
+// }
--- controller/test.php
+++ controller/test.php
@@ -1,23 +1,8 @@
 <?php
-
 include('Infirmier.php');
-function show_doctors()
-{
-    try {
-        $c = Infirmier::connect();
-        if ($c != null) {
-            //   $res =$c->query("SELECT COUNT(*)  FROM `medecin`");
-            //   echo "<script>alert($res)</script>";
-        }
-    } catch (Exception $th) {
-        echo "<script>alert($th);</script>";
-    }
-}
-$F = new Infirmier(null,null,null,null,null,null,null,null,null);
-foreach($F->afficherRDV() as $g)
-{
-    
-}
+class test{
+    use myconnect;
 
-
+}
+ test::connect();
 ?>
